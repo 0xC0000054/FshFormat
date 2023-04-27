eaaf17952220884fdb9399ac564d67a0eff784cb
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2023-04-27 07:29:27 -0600

    Prepare the project for public release
    Added README.md and License.txt.
    Added a license header to all existing files.

---
+++ README.md
@@ -0,0 +1,11 @@
+# FshFormat
+
+A file format plug-in for Adobe Photoshop® that loads and saves FSH images.
+
+# License
+
+The library is licensed under the GNU General Public License version 3.0 because it uses the DXT compression code from FSHTool.
+The FSHTool read-me indicates that it is licensed under the terms of the GNU General Public License without specifying the license version. 
+
+This project is licensed under the terms of the GNU General Public License version 3.0.   
+See [License.txt](License.txt) for more information.
--- src/version.h
+++ src/version.h
@@ -1,2 +1,23 @@
+/*
+*  This file is part of FshFormat, a file format plug-in for Adobe Photoshop(R)
+*  that loads and saves FSH images.
+*
+*  Copyright (C) 2011, 2012, 2013, 2014, 2015, 2022, 2023 Nicholas Hayes
+*
+*  This program is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
 #define VI_VERSION       1,1,4,0
 #define VERSION_STR      "1.1.4.0"
